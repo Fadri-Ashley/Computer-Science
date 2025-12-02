140acee8626017c1d750528ce6bf243d9a440b90
Fadri <fadriasliarga@gmail.com>
2025-12-02 20:56:06 +0700

    solve Nth problem

--- logic/bp8.c
+++ logic/bp8.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
 // Nth Term of AP
 
-// Unsolved
+int main() {
+
+    int a1, a2, num;
+
+    printf("Input a1: ");
+    scanf("%d", &a1);
+    printf("Input a2: ");
+    scanf("%d", &a2);
+    printf("Input num: ");
+    scanf("%d", &num);
+
+    int sum = a1, diff = a2 - a1;
+
+    for (int i = 1; i < num; i++) {
+        sum += diff;
+    }
+
+    printf("Output: %d", sum);
+
+    return 0;
+}
