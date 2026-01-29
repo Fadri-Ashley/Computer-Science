19242a9e8b0fb226cd3a33505c5a2c7cc24bc35e
Fadri <fadriasliarga@gmail.com>
2026-01-29 10:30:29 +0700

    solving is exponent

--- logic/easy/problem-4.c
+++ logic/easy/problem-4.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 // Check Exponent
 
+bool isExponent(int x, int y) {
+    if (x <= 1) {
+        return (y == 1);
+    }
+
+    while (y % x == 0) {
+        y /= x;
+    }
+
+    return (y == 1);
+}
+
 int main() {
 
+    int x, y;
 
+    printf("Input x: ");
+    scanf("%d", &x);
+    printf("Input y: ");
+    scanf("%d", &y);
+
+    if (isExponent(x, y)) {
+        printf("True");
+    } else {
+        printf("False");
+    }
 
     return 0;
 }
